ce57f310fe283c193c7c075eaf05e99ff721de29
Alistair McMillan <alistairmcmillan75@gmail.com>
2013-09-04 00:09:01 +0100

    - Fixed bug where every game but the first ended immediately in zero seconds - Fixed bug where right-clicking on face/counter area started game/incremented flag count - Tidied code - Removed jquery map call

--- game.js
+++ game.js
@@ -20,17 +20,11 @@ var EMPTY = 1;
 var BOMB = 2;
 var EXPLODED = 3;
 
-var squares = [];
-for (x = 0; x < FIELDWIDTH; x += 1) {
-    squares[x] = [];
-}
-
 var NOTGUESSED = 0;
 var GUESSED = 1;
 var FLAGGED = 2;
 var MOUSEDOWN = 3;
 var MARKED = 4;
-var guesses;				// array that holds players changes to board
 var bombsIdentified = 0;
 var squaresCleared = 0;
 var seconds = 0;
@@ -49,14 +43,61 @@ var SUBMITTINGSCORE = 0;
 
 var clickX, clickY;
 
-function resetGuesses() {
+var squares = [];			// array that holds bombs and neighbouring counts
+var guesses = [];			// array that holds players changes to board
+var checks = [];			// array that holds whether square has been checked this turn
+
+function resetArrays() {
+	var randomX, randomY, count;
+
+	squaresCleared = 0;
+
+	squares = [];
 	guesses = [];
-	for (x = 0; x < FIELDWIDTH; x += 1) {
+	checks = [];
+    for (x = 0; x < FIELDWIDTH; x += 1) {
+		squares[x] = [];
 		guesses[x] = [];
-		for (y = 0; y < FIELDHEIGHT; y += 1) {
+		checks[x] = [];
+        for (y = 0; y < FIELDHEIGHT; y += 1) {
+            squares[x][y] = EMPTY;
 			guesses[x][y] = NOTGUESSED;
-		}
-	}
+			checks[x][y] = NOTCHECKED;
+        }
+    }
+
+    // Generate bomb locations
+    x = 0;
+    while (x < NUMBEROFBOMBS) {
+        randomX = Math.floor(Math.random() * FIELDWIDTH);
+        randomY = Math.floor(Math.random() * FIELDHEIGHT);
+        if (squares[randomX][randomY] !== BOMB) {
+            squares[randomX][randomY] = BOMB;
+            x += 1;
+        }
+    }
+
+    // Generate neighbour counts
+    for (x = 0; x < FIELDWIDTH; x += 1) {
+        for (y = 0; y < FIELDHEIGHT; y += 1) {
+            if (squares[x][y] !== BOMB) {
+                count = 0;
+                count = count + isBomb(x - 1, y - 1);
+                count = count + isBomb(x, y - 1);
+                count = count + isBomb(x + 1, y - 1);
+
+                count = count + isBomb(x - 1, y);
+                count = count + isBomb(x + 1, y);
+
+                count = count + isBomb(x - 1, y + 1);
+                count = count + isBomb(x, y + 1);
+                count = count + isBomb(x + 1, y + 1);
+                if (count > 0) {
+					squares[x][y] = 4 + count;
+                }
+            }
+        }
+    }
 }
 
 function countClearedSquares() {
@@ -70,19 +111,8 @@ function countClearedSquares() {
 	}
 }
 
-var checks;					// array that holds whether square has been checked this turn
 var NOTCHECKED = 0;
 var CHECKED = 1;
-
-function resetChecks() {
-	checks = [];
-	for (x = 0; x < FIELDWIDTH; x += 1) {
-		checks[x] = [];
-		for (y = 0; y < FIELDHEIGHT; y += 1) {
-			checks[x][y] = NOTCHECKED;
-		}
-	}
-}
 
 function isBomb(i, j) {
 	var retVal = 0;
@@ -130,12 +160,12 @@ function drawField() {
 	// Drawing count of identified bombs
 	if (strlen === 3) {
 		context.drawImage(sprite, (parseInt(strBombsIdentified[0], 10))*13, 16, 13, 23, WINDOWBORDER+6, WINDOWBORDER+5, 13, 23);
-    	context.drawImage(sprite, (parseInt(strBombsIdentified[1], 10))*13, 16, 13, 23, WINDOWBORDER+19, WINDOWBORDER+5, 13, 23);
-	    context.drawImage(sprite, (parseInt(strBombsIdentified[2], 10))*13, 16, 13, 23, WINDOWBORDER+32, WINDOWBORDER+5, 13, 23);
+		context.drawImage(sprite, (parseInt(strBombsIdentified[1], 10))*13, 16, 13, 23, WINDOWBORDER+19, WINDOWBORDER+5, 13, 23);
+		context.drawImage(sprite, (parseInt(strBombsIdentified[2], 10))*13, 16, 13, 23, WINDOWBORDER+32, WINDOWBORDER+5, 13, 23);
 	} else if (strlen === 2) {
 		context.drawImage(sprite, 0, 16, 13, 23, WINDOWBORDER+6, WINDOWBORDER+5, 13, 23);
 		context.drawImage(sprite, (parseInt(strBombsIdentified[0], 10))*13, 16, 13, 23, WINDOWBORDER+19, WINDOWBORDER+5, 13, 23);
-    	context.drawImage(sprite, (parseInt(strBombsIdentified[1], 10))*13, 16, 13, 23, WINDOWBORDER+32, WINDOWBORDER+5, 13, 23);
+		context.drawImage(sprite, (parseInt(strBombsIdentified[1], 10))*13, 16, 13, 23, WINDOWBORDER+32, WINDOWBORDER+5, 13, 23);
 	} else if (strlen === 1) {
 		context.drawImage(sprite, 0, 16, 13, 23, WINDOWBORDER+6, WINDOWBORDER+5, 13, 23);
 		context.drawImage(sprite, 0, 16, 13, 23, WINDOWBORDER+19, WINDOWBORDER+5, 13, 23);
@@ -144,11 +174,11 @@ function drawField() {
 
 	// Drawing seconds counter
 	if(seconds < 1000) {
-	    strSeconds = String(seconds);
+		strSeconds = String(seconds);
     } else {
 		strSeconds = String(999);
     }
-   	strlen = strSeconds.length;
+	strlen = strSeconds.length;
 	if (strlen === 3) {
 		context.drawImage(sprite, (parseInt(strSeconds[0], 10))*13, 16, 13, 23, CANVASWIDTH-WINDOWBORDER-45, WINDOWBORDER+5, 13, 23);
 		context.drawImage(sprite, (parseInt(strSeconds[1], 10))*13, 16, 13, 23, CANVASWIDTH-WINDOWBORDER-32, WINDOWBORDER+5, 13, 23);
@@ -178,8 +208,8 @@ function drawField() {
 	// Drawing actual minefield
     for (x = 0; x < FIELDWIDTH; x += 1) {
         for (y = 0; y < FIELDHEIGHT; y += 1) {
-        	// Adding 33 to give space for the bombs and time counters
-        	// Add 13 to x and y to give space for border
+			// Adding 33 to give space for the bombs and time counters
+			// Add 13 to x and y to give space for border
             if (guesses[x][y] === MOUSEDOWN) {
                 context.drawImage(sprite, 16, 0, 16, 16, (x * 16) + WINDOWBORDER, (y * 16) + (WINDOWBORDER * 2) + SCOREHEIGHT, 16, 16);
             } else if (guesses[x][y] === MARKED) {
@@ -396,11 +426,16 @@ function userMousedDown(evt) {
 	clickX = Math.floor(((evt.pageX - canvas.offsetLeft) - WINDOWBORDER) / 16);
 	clickY = Math.floor(((evt.pageY - canvas.offsetTop) - (WINDOWBORDER * 2) - SCOREHEIGHT) / 16);
 
-	if (GAMESTATUS === GAMEPLAYING) {
-		if (guesses[clickX][clickY] === NOTGUESSED) {
-			guesses[clickX][clickY] = MOUSEDOWN;
+	if ((evt.pageX - canvas.offsetLeft) > WINDOWBORDER &&
+		(evt.pageX - canvas.offsetLeft) < CANVASWIDTH-WINDOWBORDER &&
+		(evt.pageY - canvas.offsetTop) > 55 &&
+		(evt.pageY - canvas.offsetTop) < CANVASHEIGHT-WINDOWBORDER ) {
+		if (GAMESTATUS === GAMEPLAYING) {
+			if (guesses[clickX][clickY] === NOTGUESSED) {
+				guesses[clickX][clickY] = MOUSEDOWN;
+			}
+			mousebtnheld = 1;
 		}
-		mousebtnheld = 1;
 	}
     drawField();
 }
@@ -417,58 +452,68 @@ function userMousedUp(evt) {
 			guesses[clickX][clickY] = NOTGUESSED;
 		}
 	} else {
-		// If this is the first move, start the timer
-		if (GAMESTATUS === NOGAME) {
-			GAMESTATUS = GAMEPLAYING;
-			tmr = setInterval(function(){clock();},1000);
-		}
-
 		// If right-click
 		if (evt.button === 2) {
-			// Only let the player make changes when the game is still going
-			if (GAMESTATUS === GAMEPLAYING) {
-				if (guesses[clickX][clickY] === MARKED) {
-					guesses[clickX][clickY] = NOTGUESSED;
-				} else if (guesses[clickX][clickY] === FLAGGED) {
-					if (MARKSON) {
-						guesses[clickX][clickY] = MARKED;
-						bombsIdentified -= 1;
-					} else {
+			// If clicked on game board
+			if ((evt.pageX - canvas.offsetLeft) > WINDOWBORDER &&
+				(evt.pageX - canvas.offsetLeft) < CANVASWIDTH-WINDOWBORDER &&
+				(evt.pageY - canvas.offsetTop) > 55 &&
+				(evt.pageY - canvas.offsetTop) < CANVASHEIGHT-WINDOWBORDER ) {
+				if (GAMESTATUS === GAMEPLAYING) { // Only let the player make changes when the game is still going
+					if (guesses[clickX][clickY] === MARKED) {
 						guesses[clickX][clickY] = NOTGUESSED;
-						bombsIdentified -= 1;
+					} else if (guesses[clickX][clickY] === FLAGGED) {
+						if (MARKSON) {
+							guesses[clickX][clickY] = MARKED;
+							bombsIdentified -= 1;
+						} else {
+							guesses[clickX][clickY] = NOTGUESSED;
+							bombsIdentified -= 1;
+						}
+					} else if (guesses[clickX][clickY] === GUESSED) {
+						// Do nothing to squares that have already been cleared
+					} else {
+						guesses[clickX][clickY] = FLAGGED;
+						bombsIdentified += 1;
 					}
-				} else if (guesses[clickX][clickY] === GUESSED) {
-					// Do nothing to squares that have already been cleared
-				} else {
-					guesses[clickX][clickY] = FLAGGED;
-					bombsIdentified += 1;
 				}
 			}
+			/*
+			*/
 		// If left-click
 		} else {
-			if ((evt.pageX - canvas.offsetLeft) > (CANVASWIDTH/2)-13 && (evt.pageX - canvas.offsetLeft) < (CANVASWIDTH/2)+13 && (evt.pageY - canvas.offsetTop) > WINDOWBORDER+3 && (evt.pageY - canvas.offsetTop) < +WINDOWBORDER+29 ) {
+			// If clicked on face
+			if ((evt.pageX - canvas.offsetLeft) > (CANVASWIDTH/2)-13 &&
+				(evt.pageX - canvas.offsetLeft) < (CANVASWIDTH/2)+13 &&
+				(evt.pageY - canvas.offsetTop) > WINDOWBORDER+3 &&
+				(evt.pageY - canvas.offsetTop) < WINDOWBORDER+29 ) {
 				newGame(CURRENTGAME);
-			} else if (squares[clickX][clickY] === BOMB) {
-				if (GAMESTATUS === GAMEPLAYING) {
-					for (x = 0; x < FIELDWIDTH; x += 1) {
-						for (y = 0; y < FIELDHEIGHT; y += 1) {
-							guesses[x][y] = GUESSED;
-						}
-					}
-					squares[clickX][clickY] = EXPLODED;
-					GAMESTATUS = GAMELOST;
-					clearInterval(tmr);
-				}
 			} else {
-				// if game isn't over and
-				// square is currently hidden
-				if (GAMESTATUS === GAMEPLAYING && guesses[clickX][clickY] !== GUESSED) {
-					guesses[clickX][clickY] = GUESSED;
-					// Check for adjacent blanks is clicked square is empty
-					if(squares[clickX][clickY] === EMPTY) {
-						checkForAdjacentBlanks(clickX, clickY);
+				// If this is the first move, start the timer
+				if (GAMESTATUS !== GAMEPLAYING) {
+					// If this is the first move, start the timer
+					GAMESTATUS = GAMEPLAYING;
+					tmr = setInterval(function(){clock();},1000);
+				}
+
+				if (GAMESTATUS === GAMEPLAYING) {
+					if (squares[clickX][clickY] === BOMB) {
+						for (x = 0; x < FIELDWIDTH; x += 1) {
+							for (y = 0; y < FIELDHEIGHT; y += 1) {
+								guesses[x][y] = GUESSED;
+							}
+						}
+						squares[clickX][clickY] = EXPLODED;
+						GAMESTATUS = GAMELOST;
+						clearInterval(tmr);
+					} else if (guesses[clickX][clickY] !== GUESSED) {
+						guesses[clickX][clickY] = GUESSED;
+						// Check for adjacent blanks is clicked square is empty
+						if(squares[clickX][clickY] === EMPTY) {
+							checkForAdjacentBlanks(clickX, clickY);
+						}
+						countClearedSquares();
 					}
-					countClearedSquares();
 				}
 			}
 		}
@@ -517,8 +562,6 @@ function userKeyUp(evt) {
 // I: 16 × 16 field with 40 mines and 256 x 256 grid
 // E: 30 × 16 field with 99 mines and 480 x 256 grid
 function newGame(size) {
-    var randomX, randomY, count;
-
 	bombsIdentified = 0;
 	seconds = 0;
 	if(tmr) {
@@ -558,13 +601,7 @@ function newGame(size) {
 	canvas.height = CANVASHEIGHT;
     context = canvas.getContext('2d');
 
-	squares = [];
-	for (x = 0; x < FIELDWIDTH; x += 1) {
-		squares[x] = [];
-	}
-	resetGuesses();
-
-	resetChecks();
+	resetArrays();
 
     canvas.addEventListener("contextmenu", userRightClicked, false);
     canvas.addEventListener("mousedown", userMousedDown, false);
@@ -575,48 +612,8 @@ function newGame(size) {
     sprite = new Image();
     sprite.src = 'mines.png';
 
-    for (x = 0; x < FIELDWIDTH; x += 1) {
-        for (y = 0; y < FIELDHEIGHT; y += 1) {
-            squares[x][y] = EMPTY;
-        }
-    }
-
-    // Generate bomb locations
-    x = 0;
-    while (x < NUMBEROFBOMBS) {
-        randomX = Math.floor(Math.random() * FIELDWIDTH);
-        randomY = Math.floor(Math.random() * FIELDHEIGHT);
-        if (squares[randomX][randomY] !== BOMB) {
-            squares[randomX][randomY] = BOMB;
-            x += 1;
-        }
-    }
-
-    // Generate neighbour counts
-    for (x = 0; x < FIELDWIDTH; x += 1) {
-        for (y = 0; y < FIELDHEIGHT; y += 1) {
-            if (squares[x][y] !== BOMB) {
-                count = 0;
-                count = count + isBomb(x - 1, y - 1);
-                count = count + isBomb(x, y - 1);
-                count = count + isBomb(x + 1, y - 1);
-
-                count = count + isBomb(x - 1, y);
-                count = count + isBomb(x + 1, y);
-
-                count = count + isBomb(x - 1, y + 1);
-                count = count + isBomb(x, y + 1);
-                count = count + isBomb(x + 1, y + 1);
-                if (count > 0) {
-                	squares[x][y] = 4 + count;
-                }
-            }
-        }
-    }
-
     // Draw minefield
     sprite.onload = function() {
         drawField();
     };
-
 }
